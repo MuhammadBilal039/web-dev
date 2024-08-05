acade24d68b433ab796993864db535cf537871e7
Muhammad Bilal <150532599+MuhammadBilal039@users.noreply.github.com>
2024-08-05 23:02:41 +0500

    Add files via upload

---
+++ JavaScript Introduction - day-30/main.js
@@ -0,0 +1,8 @@
+let age = 22;
+let userName = "Bilal Khan";
+let hobbies = ["Sports", "Programming", "Traveling", "Reciting"];
+let job = { title: "Web Developer", name: "ABC", salary: 50000 };
+
+alert(age);
+alert(userName);
+alert(hobbies[3]);
