39d932492b518bc13677d744e620690d40d44258
Muhammad Bilal <150532599+MuhammadBilal039@users.noreply.github.com>
2024-08-07 23:10:53 +0500

    Add files via upload

---
+++ day-31/main.js
@@ -0,0 +1,18 @@
+let age = 32;
+let userName = "Bilal Khan";
+let hobbies = ["Sports", "Programming", "Traveling", "Reciting"];
+let job = { title: "Web Developer", name: "ABC", salary: 50000 };
+
+let totalAdultYears;
+
+function calculateAdultYears(userAge) {
+  let result = userAge - 18;
+  return result;
+}
+
+totalAdultYears = calculateAdultYears(age);
+alert(totalAdultYears);
+
+age = 45;
+totalAdultYears = calculateAdultYears(age);
+alert(totalAdultYears);
